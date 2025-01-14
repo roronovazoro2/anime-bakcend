af7757adc42ce788c387e6c6828eedf8cae8d5c5
roronovazoro2 <monkeydluffy_joy@proton.me>
2025-01-14 17:30:35 +0530

    Update index.js

--- index.js
+++ index.js
@@ -62,9 +62,11 @@ const fetchTrendingAnime = async () => {
 
 fetchTrendingAnime();
 
-app.listen(3000, () => {
-  console.log("Server is running on port 3000");
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
 });
+
 
 app.get("/", (req, res) => {
   res.send("Hello World something");
